fc5b574846b23bf5f33335925f7fab8db1c970ec
chestarpro <chestarpro@gmail.com>
2021-05-22 04:46:50 +0600

    removing comments

--- Task1/src/com/company/Main.java
+++ Task1/src/com/company/Main.java
@@ -9,12 +9,13 @@ public class Main {
 
 		Circle[] circles = new Circle[15];
 
-	    for (int i = 0; i < circles.length; i++) { // Не пойму зачем такие усливия от 1 до 10 радиусы xD Ведь там нету кратных 3 площадей.
-	        circles[i] = new Circle(random.nextInt(10 + 1 - 1) + 1); // Я оставил все как в условии заднии. Но проверял от 1 до 1000 все нормально работает.
-	        if(circles[i].getArea() % 3 == 0) { // Так для разнообразия сдела :) Тут просто индекс круга вывод с информацией если площадь кратна 3.
+	    for (int i = 0; i < circles.length; i++) {
+	        circles[i] = new Circle(random.nextInt(10 + 1 - 1) + 1);
+	        if(circles[i].getArea() % 3 == 0) {
 				System.out.printf("Circle index[%s]: radius = %s, area = %s \n", i, circles[i].getRadius(), circles[i].getArea());
 			}
 	    }
+
         System.out.println("\nThe number of circles whose area is a multiple of three = " + Circle.getCountAreaMultOfThree());
     }
 }
